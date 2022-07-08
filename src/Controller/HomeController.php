bf3d9f75fd4b858cca4a04c5bdd395f24cb94333
Ron Chaplin <ron.g.chaplin@gmail.com>
2022-07-07 21:27:34 -0500

    Cleaning up controller and views

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class HomeController
+ *
+ * This is the base controller that handles the initial page rendering for the React JS app.
+ */
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    /**
+     * The main endpoint for initial page rendering.
+     *
+     * @Route("/")
+     *
+     * @return Response
+     */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
